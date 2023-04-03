b471c279241ecb6c48742855e4e4c7cb61ae02e3
Marco <verduci.marco2099@gmail.com>
2023-04-03 17:56:33 +0200

    commit

---
+++ 13 Array Methods Advanced/exercise-3.js
@@ -0,0 +1,22 @@
+function calculateAverageAge(persons) {
+  let age = persons.reduce((avg, person) => avg + person.age, 0);
+  let avgAge = age / persons.length;
+  return avgAge;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
