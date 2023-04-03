c43932ad06d42a36e437be1d24d073f4e166a3b9
Marco <verduci.marco2099@gmail.com>
2023-04-03 17:29:52 +0200

    commit

---
+++ 09 Objects Methods/exercise-1.js
@@ -0,0 +1,12 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+let keys = Object.keys(person);
+let values = Object.values(person);
+console.log(keys);
+console.log(values);
+
+// Print values of person using Object.keys
